61f2679c4540767185319e840397b494a18b17ac
Trenton Sims <tsims115@yahoo.com>
2023-02-18 13:49:23 -0600

    Adding task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,6 @@
 void outer(String name, String id) {
     String inner() {
-        String str = "Hello Agent ${name.split(' ')[1][0]}.${name.split(' ')[0]} your id is ${id}\n";
+        String str = "Hello Agent ${name.split(' ')[1][0]}.${name.split(' ')[0]} your id is ${id}";
         return str;
     }
     print(inner());
