68af6344e43a73881fba57877f919038219f2ca9
Trenton Sims <tsims115@yahoo.com>
2023-02-18 14:22:32 -0600

    Adding task 8

---
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+String longestUniqueSubstring(String str) {
+    if (str == "") {
+        return "";
+    }
+    int start = 0;
+    int end = 0;
+    int maxLength = 0;
+    Set<String> unique_characters = {};
+    while (end < str.length) {
+        if (!(unique_characters.any((c) => c == str[end]))) {
+            unique_characters.add(str[end]);
+            end += 1;
+            maxLength = max(maxLength, unique_characters.length);
+        }
+        else {
+            unique_characters.remove(str[start]);
+            start += 1;
+        }
+    }
+    String unique = "";
+    for (var s in unique_characters) {
+        unique += s;
+    }
+    return unique;
+}
