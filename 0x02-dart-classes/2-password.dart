d3f8f1c99a0f8a09e9856cdb5e9b3e9d82c966e7
Trenton Sims <tsims115@yahoo.com>
2023-02-19 12:36:06 -0600

    Adding task 2

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,14 +2,10 @@ class Password {
   String password;
   Password({this.password: ""});
   bool isValid() {
-    int length = this.password.length;
-    final numericRegex = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$');
-    if (length > 16) {
-      return false;
-    }
-    return numericRegex.hasMatch(this.password);
+    return RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$')
+      .hasMatch(this.password);
   }
-  
+
   @override
   String toString() {
     return "Your Password is: ${this.password}";
