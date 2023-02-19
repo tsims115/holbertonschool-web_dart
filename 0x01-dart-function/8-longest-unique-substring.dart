f6a8990c16da9ae0b0f256374d66ffcfd4829198
Trenton Sims <tsims115@yahoo.com>
2023-02-18 22:09:54 -0600

    Adding task 8

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,33 +1,34 @@
 import 'dart:math';
 
 String longestUniqueSubstring(String str) {
+    String createString(Set<String> unique_characters) {
+        String unique = "";
+        for (var s in unique_characters) {
+            unique += s;
+        }
+        return unique;
+    }
     if (str == "") {
         return "";
-    }
-    if (str[0] == "b") {
-        return "ban";
-    }
-    if (str[2] == "a") {
-        return "aZz";
     }
     int start = 0;
     int end = 0;
     int maxLength = 0;
+    String unique = "";
+    String maxString = "";
     Set<String> unique_characters = {};
     while (end < str.length) {
         if (!(unique_characters.any((c) => c == str[end]))) {
             unique_characters.add(str[end]);
             end += 1;
-            maxLength = max(maxLength, unique_characters.length);
         }
         else {
             unique_characters.remove(str[start]);
             start += 1;
         }
-    }
-    String unique = "";
-    for (var s in unique_characters) {
-        unique += s;
+        if (unique_characters.length > unique.length) {
+            unique = createString(unique_characters);
+        }
     }
     return unique;
 }
