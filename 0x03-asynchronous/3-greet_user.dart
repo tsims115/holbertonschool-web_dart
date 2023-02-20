6e9627b021b0975e7d6140fec4abc48e7dce90ab
Trenton Sims <tsims115@yahoo.com>
2023-02-19 18:50:59 -0600

    Adding task 3

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,4 +1,11 @@
-import '3-main.dart';
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
 
 Future<String> greetUser() async {
   try {
