122ac81f2c9090ac106d0abf0f4aeaff0f370a0f
Trenton Sims <tsims115@yahoo.com>
2023-02-20 06:40:53 -0600

    Adding task 5

--- 0x03-asynchronous/breaking_bad.dart
+++ 0x03-asynchronous/breaking_bad.dart
@@ -2,4 +2,5 @@ import 'package:http/http.dart' as http;
 import 'dart:core';
 Future<void> printBbCharacters() async {
   var response = await http.get(Uri.parse("https://breakingbadapi.com/characters"));
+  
 }
